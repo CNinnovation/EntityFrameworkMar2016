843ba461f0fc13d8b32c320fc191a0e6fe0af82f
Christian Nagel <christian@christiannagel.com>
2016-03-07 13:16:05 +0100

    relationship sample

---
+++ RelationSample/RelationSample/MenuCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Wrox.ProCSharp.Entities
+{
+    public class MenuCard
+    {
+        public MenuCard()
+        {
+            Menus = new HashSet<Menu>();
+        }
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
---
+++ RelationSample/RelationSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wrox.ProCSharp.Entities;
+
+namespace RelationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generate();
+        }
+
+        private static void Generate()
+        {
+            using (var context = new MenuContext())
+            {
+                MenuCard card = context.MenuCards.Create();
+                card.Text = "Mittagsmenüs";
+                Menu m1 = new Menu() { Day = DateTime.Today, Text = "Wiener Schnitzel mit Kartoffelsalat", Price = 8.9m, MenuCard = card };
+                card.Menus.Add(m1);
+                context.Menus.Add(m1);
+                context.MenuCards.Add(card);
+                int changed = context.SaveChanges();
+            }
+        }
+    }
+}
