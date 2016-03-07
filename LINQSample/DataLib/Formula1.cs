0c607731781c19380cc1d48fc1b057bc20f51f9f
Christian Nagel <christian@christiannagel.com>
2016-03-07 10:49:31 +0100

    LINQ sample

---
+++ LINQSample/DataLib/Formula1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLib
+{
+    public static class Formula1
+    {
+        private static List<Racer> _racers;
+
+        public static IList<Racer> GetChampions()
+        {
+            if (_racers == null)
+            {
+                _racers = new List<Racer>(40);
+                _racers.Add(new Racer("Nino", "Farina", "Italy", 33, 5, new int[] { 1950 }, new string[] { "Alfa Romeo" }));
+                _racers.Add(new Racer("Alberto", "Ascari", "Italy", 32, 10, new int[] { 1952, 1953 }, new string[] { "Ferrari" }));
+                _racers.Add(new Racer("Juan Manuel", "Fangio", "Argentina", 51, 24, new int[] { 1951, 1954, 1955, 1956, 1957 }, new string[] { "Alfa Romeo", "Maserati", "Mercedes", "Ferrari" }));
+                _racers.Add(new Racer("Mike", "Hawthorn", "UK", 45, 3, new int[] { 1958 }, new string[] { "Ferrari" }));
+                _racers.Add(new Racer("Phil", "Hill", "USA", 48, 3, new int[] { 1961 }, new string[] { "Ferrari" }));
+                _racers.Add(new Racer("John", "Surtees", "UK", 111, 6, new int[] { 1964 }, new string[] { "Ferrari" }));
+                _racers.Add(new Racer("Jim", "Clark", "UK", 72, 25, new int[] { 1963, 1965 }, new string[] { "Lotus" }));
+                _racers.Add(new Racer("Jack", "Brabham", "Australia", 125, 14, new int[] { 1959, 1960, 1966 }, new string[] { "Cooper", "Brabham" }));
+                _racers.Add(new Racer("Denny", "Hulme", "New Zealand", 112, 8, new int[] { 1967 }, new string[] { "Brabham" }));
+                _racers.Add(new Racer("Graham", "Hill", "UK", 176, 14, new int[] { 1962, 1968 }, new string[] { "BRM", "Lotus" }));
+                _racers.Add(new Racer("Jochen", "Rindt", "Austria", 60, 6, new int[] { 1970 }, new string[] { "Lotus" }));
+                _racers.Add(new Racer("Jackie", "Stewart", "UK", 99, 27, new int[] { 1969, 1971, 1973 }, new string[] { "Matra", "Tyrrell" }));
+                _racers.Add(new Racer("Emerson", "Fittipaldi", "Brazil", 143, 14, new int[] { 1972, 1974 }, new string[] { "Lotus", "McLaren" }));
+                _racers.Add(new Racer("James", "Hunt", "UK", 91, 10, new int[] { 1976 }, new string[] { "McLaren" }));
+                _racers.Add(new Racer("Mario", "Andretti", "USA", 128, 12, new int[] { 1978 }, new string[] { "Lotus" }));
+                _racers.Add(new Racer("Jody", "Scheckter", "South Africa", 112, 10, new int[] { 1979 }, new string[] { "Ferrari" }));
+                _racers.Add(new Racer("Alan", "Jones", "Australia", 115, 12, new int[] { 1980 }, new string[] { "Williams" }));
+                _racers.Add(new Racer("Keke", "Rosberg", "Finland", 114, 5, new int[] { 1982 }, new string[] { "Williams" }));
+                _racers.Add(new Racer("Niki", "Lauda", "Austria", 173, 25, new int[] { 1975, 1977, 1984 }, new string[] { "Ferrari", "McLaren" }));
+                _racers.Add(new Racer("Nelson", "Piquet", "Brazil", 204, 23, new int[] { 1981, 1983, 1987 }, new string[] { "Brabham", "Williams" }));
+                _racers.Add(new Racer("Ayrton", "Senna", "Brazil", 161, 41, new int[] { 1988, 1990, 1991 }, new string[] { "McLaren" }));
+                _racers.Add(new Racer("Nigel", "Mansell", "UK", 187, 31, new int[] { 1992 }, new string[] { "Williams" }));
+                _racers.Add(new Racer("Alain", "Prost", "France", 197, 51, new int[] { 1985, 1986, 1989, 1993 }, new string[] { "McLaren", "Williams" }));
+                _racers.Add(new Racer("Damon", "Hill", "UK", 114, 22, new int[] { 1996 }, new string[] { "Williams" }));
+                _racers.Add(new Racer("Jacques", "Villeneuve", "Canada", 165, 11, new int[] { 1997 }, new string[] { "Williams" }));
+                _racers.Add(new Racer("Mika", "Hakkinen", "Finland", 160, 20, new int[] { 1998, 1999 }, new string[] { "McLaren" }));
+                _racers.Add(new Racer("Michael", "Schumacher", "Germany", 287, 91, new int[] { 1994, 1995, 2000, 2001, 2002, 2003, 2004 }, new string[] { "Benetton", "Ferrari" }));
+                _racers.Add(new Racer("Fernando", "Alonso", "Spain", 252, 32, new int[] { 2005, 2006 }, new string[] { "Renault" }));
+                _racers.Add(new Racer("Kimi", "Räikkönen", "Finland", 230, 20, new int[] { 2007 }, new string[] { "Ferrari" }));
+                _racers.Add(new Racer("Lewis", "Hamilton", "UK", 166, 43, new int[] { 2008, 2014, 2015 }, new string[] { "McLaren", "Mercedes" }));
+                _racers.Add(new Racer("Jenson", "Button", "UK", 283, 15, new int[] { 2009 }, new string[] { "Brawn GP" }));
+                _racers.Add(new Racer("Sebastian", "Vettel", "Germany", 156, 42, new int[] { 2010, 2011, 2012, 2013 }, new string[] { "Red Bull Racing" }));
+            }
+
+            return _racers;
+        }
+
+
+     
+    }
+}
---
+++ LINQSample/LINQSample/Program.cs
@@ -0,0 +1,111 @@
+﻿using DataLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSample
+{
+    static class StringExtensions
+    {
+        public static void Foo(this string s, int x)
+        {
+            Console.WriteLine($"s: {s}, x: {x}");
+        }
+    }
+
+    static class CollectionExtensions
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqQuery();
+            // LinqMethodSyntax();
+            // GroupingSample();
+            // UseMyExtensionMethod();
+
+            string s = "sample";
+            s.Foo(42);
+        }
+
+        private static void UseMyExtensionMethod()
+        {
+            var austrianRacers = Formula1.GetChampions().Filter(r => r.Country == "Austria");
+
+            foreach (var racer in austrianRacers)
+            {
+                Console.WriteLine($"{racer.FirstName} {racer.LastName}");
+            }
+        }
+
+        private static void GroupingSample()
+        {
+            var q = (from r in Formula1.GetChampions()
+                    group r by r.Country into g
+                    let count = g.Count()
+                    orderby count descending, g.Key
+                    select new 
+                    {
+                        Country = g.Key,
+                        Count = count
+                    }).Take(5);
+
+            foreach (var item in q)
+            {
+                Console.WriteLine($"{item.Country} {item.Count}");
+            }
+
+        }
+
+        private static void LinqMethodSyntax()
+        {
+            var q = Formula1.GetChampions()
+                .Where(r => r.Country == "Brazil")
+                .OrderByDescending(r => r.Wins)
+                .Select(r => r);
+
+            foreach (var r in q)
+            {
+                Console.WriteLine($"{r.FirstName} {r.LastName} {r.Wins}");
+            }
+        }
+
+        private static void LinqQuery()
+        {
+            var q = from r in Formula1.GetChampions()
+                    where r.Country == "Brazil"
+                    orderby r.Wins descending
+                    select r;
+
+            foreach (var r in q)
+            {
+                Console.WriteLine($"{r.FirstName} {r.LastName} {r.Wins}");
+            }
+        }
+    }
+}
