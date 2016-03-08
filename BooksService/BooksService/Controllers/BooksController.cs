5c4a0448b98f3de20d7b0fbc0e84d0d82f10e07a
Christian Nagel <christian@christiannagel.com>
2016-03-08 11:09:32 +0100

    books service and client

---
+++ BooksService/BooksService/Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using BooksService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BooksService.Controllers
+{
+    [AllowAnonymous]
+    public class BooksController : ApiController
+    {
+        private static List<Book> books = new List<Book>()
+        {
+            new Book { BookId = 1, Title = "Professional C# 5"},
+            new Book { BookId = 2, Title = "Professional C# 6"}
+        };
+
+        // GET api/values
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        // GET api/values/5
+        public Book Get(int id)
+        {
+            return books[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]Book value)
+        {
+            books.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Book value)
+        {
+            books[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            books.RemoveAt(id);
+        }
+    }
+}
---
+++ BooksService/ClientAppFramework/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientAppFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("client - wait for server to start");
+            Console.ReadLine();
+            GetSample();
+            Console.WriteLine("Main, wait for answer");
+            Console.ReadLine();
+        }
+
+        private static async void GetSample()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://localhost:3782/api/books");
+            response.EnsureSuccessStatusCode();
+            string json = await response.Content.ReadAsStringAsync();
+            IEnumerable<Book> books = JsonConvert.DeserializeObject<IEnumerable<Book>>(json);
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+    }
+}
